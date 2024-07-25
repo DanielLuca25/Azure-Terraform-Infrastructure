b15ea485fca609ade74fecec2516195505641bbf
DanielLuca25 <danielluca67@gmail.com>
2024-07-25 15:07:31 +0300

    migratet to local

--- provider.tf
+++ provider.tf
@@ -6,13 +6,13 @@ terraform{
       }
     }
 
-    backend "azurerm"{
-        subscription_id = "76ee20cb-86ed-48e1-a2a8-abc9d6ed9969"
-        resource_group_name = "rg-terraform"
-        storage_account_name = "terraformstgacc25"
-        container_name = "terraformstgcont"
-        key = "terraform.tfstate"
-    }
+    # backend "azurerm"{
+    #     subscription_id = "76ee20cb-86ed-48e1-a2a8-abc9d6ed9969"
+    #     resource_group_name = "rg-terraform"
+    #     storage_account_name = "terraformstgacc25"
+    #     container_name = "terraformstgcont"
+    #     key = "terraform.tfstate"
+    # }
 
 }
 
